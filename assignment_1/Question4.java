d789856f38188b738fba186f92581ec1e051808b
Baky Velmammedov <71409138+bakyvelmammedov@users.noreply.github.com>
2020-10-13 01:56:24 +0400

    Add files via upload

---
+++ assignment_1/Question4.java
@@ -0,0 +1,11 @@
+package assignments.assignment_1;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Q");
+
+	}
+
+}
---
+++ assignment_1/Question8.java
@@ -0,0 +1,20 @@
+package assignments.assignment_1;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("******          *************       **********");
+		System.out.println("**    **               **           **");
+		System.out.println("**     **              **           **");
+		System.out.println("**      **             **           **");
+		System.out.println("**      **             **           ********");
+		System.out.println("**      **      **     **           **");
+		System.out.println("**     **        **    **           **");
+		System.out.println("**    **          **  **            **");
+		System.out.println("******             ****             **********");
+		
+
+	}
+
+}
