7391632a3091d1b8b940fbdf87d8adff3d55a02a
Baky Velmammedov <71409138+bakyvelmammedov@users.noreply.github.com>
2020-10-15 00:54:10 +0400

    Update Question2.java

--- Lab1_assignment/Question2.java
+++ Lab1_assignment/Question2.java
@@ -14,13 +14,13 @@ public class Question2 {
 	public static void love6(int num1, int num2) {
 		
 		if(num1==6 || num2==6) {
-			System.out.println("true");
+			System.out.println(true);
 		}else if(num1-num2==6 || num2-num1==6) {
-			System.out.println("true");
+			System.out.println(true);
 		}else if(num1+num2==6 || num2+num1==6) {
-			System.out.println("true");
+			System.out.println(true);
 		}else {
-			System.out.println("false");
+			System.out.println(false);
 		}
 		
 	}
