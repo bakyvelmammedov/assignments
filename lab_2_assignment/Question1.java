7e7115438e310b2bbbf409bc6e07eee00cf9c935
Baky Velmammedov <71409138+bakyvelmammedov@users.noreply.github.com>
2020-10-23 21:37:28 +0400

    Update Question1.java

--- lab_2_assignment/Question1.java
+++ lab_2_assignment/Question1.java
@@ -6,23 +6,42 @@ public class Question1 {
 
 	public static void main(String[] args) {
 		
+		int smallest = 0;
+		
+		int largest = 0;
+		
+		int yesOrNo;
+		
 		Scanner scan = new Scanner(System.in);
+		
 		System.out.println("Enter your first number: ");
-		int j = scan.nextInt();
+		
+		int input = scan.nextInt();
+		
+		smallest = input;
+		
+		largest = input;
 		
 		do {
 			System.out.println("Enter your next number: ");
-			int k = scan.nextInt();
+			int input2 = scan.nextInt();
+			
+			if(smallest>=input2) {		//if input1(smallest) is greater than input2
+				smallest=input2;	//my smallest number will be input 2
+			}else if(largest<=input2){	//if input1(largest) is less than input2
+				largest=input2;		//my largest number will be input2
+			}
+			
 			System.out.println("Do you want to enter another number: 0-No, 1-Yes ");
-			int l = scan.nextInt();
-			if(l==0) {
-				System.out.println("The smallest number is: " + Math.min(k, j));	//finds smallest number
-				System.out.println("The largest number is: " + Math.max(k, j));		//finds largest number
+			yesOrNo = scan.nextInt();
+			
+			if(yesOrNo==0) {
+				System.out.println("The smallest number is: " + smallest);	//define smallest number
+				System.out.println("The largest number is: " + largest);	//define largest number
 				return;		//ends loop
 			}
-		}while(true);		//if l!=0-->condition is true that's why loop is repeating 
-		
-		
+		}while(yesOrNo==1);		//if l!=0-->condition is true that's why loop is repeating 
+
 
 	}
 
